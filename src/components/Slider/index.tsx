1ea4ea02645b458968f65c729128de6d083ccccc
Anton Pomogaev <antonpomogaev@gmail.com>
2023-02-19 18:31:25 +0300

    feat: add touch support

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -14,17 +14,16 @@ const Slider = () => {
   const sliderRef = useRef<HTMLDivElement>(null);
   const imageRefMap = useRef(new Map<string, HTMLImageElement>());
 
-  const handleDragStart = (
-    e: React.MouseEvent<HTMLImageElement, MouseEvent>
-  ) => {
+  const handleDragStart = (e: React.PointerEvent<HTMLDivElement>) => {
     if (!sliderRef.current) return;
 
+    sliderRef.current.setPointerCapture(e.pointerId);
     setIsDragging(true);
     setPrevScrollLeft(sliderRef.current.scrollLeft);
     setPrevClientX(e.clientX);
   };
 
-  const handleDragEnd = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+  const handleDragEnd = (e: React.PointerEvent<HTMLDivElement>) => {
     if (!sliderRef.current) return;
 
     setIsDragging(false);
@@ -32,11 +31,11 @@ const Slider = () => {
     setPrevScrollLeft(sliderRef.current.scrollLeft);
   };
 
-  const handleDrag = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+  const handleDrag = (e: React.PointerEvent<HTMLDivElement>) => {
+    e.preventDefault();
+
     if (!isDragging) return;
     if (!sliderRef.current) return;
-
-    e.preventDefault();
 
     const change = e.clientX - prevClientX;
     const newScrollLeft = prevScrollLeft - change;
@@ -93,25 +92,30 @@ const Slider = () => {
         <button
           type="button"
           onClick={() => handleSwipe(calculateScrollAfterSwipeLeft)}
-          className="absolute block aspect-square w-8 -translate-x-1/2 cursor-pointer self-center rounded-full bg-neutral-100 text-neutral-700 transition hover:bg-indigo-500 hover:text-white"
+          className="absolute flex aspect-square w-8 -translate-x-1/2 cursor-pointer items-center justify-center self-center rounded-full bg-neutral-100 text-neutral-700 transition hover:bg-indigo-500 hover:text-white"
         >
           <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
         </button>
       )}
 
       <div
+        ref={sliderRef}
+        onPointerDown={handleDragStart}
+        onPointerMove={handleDrag}
+        onPointerUp={handleDragEnd}
+        onPointerLeave={handleDragEnd}
+        onPointerCancel={handleDragEnd}
+        // Touch-none property is very important to enable,
+        // as it allows us to freely control element scroll on touch devices.
+        // Without it browser cancels drag automatically after a short time.
+        // idk why it should be controlled by CSS tho ¯\_(ツ)_/¯
         className={`${
           isDragging
             ? // Set scroll to auto while dragging
               // to fix scroll lags
               'cursor-grabbing scroll-auto'
             : 'cursor-grab scroll-smooth'
-        } flex max-w-sm gap-x-4 overflow-hidden md:max-w-4xl lg:max-w-7xl`}
-        ref={sliderRef}
-        onMouseDown={handleDragStart}
-        onMouseMove={handleDrag}
-        onMouseUp={handleDragEnd}
-        onMouseLeave={handleDragEnd}
+        } flex max-w-sm touch-none gap-x-4 overflow-hidden md:max-w-4xl lg:max-w-7xl`}
       >
         {images.map(({ id, src }) => (
           <img
@@ -125,9 +129,9 @@ const Slider = () => {
             key={id}
             src={src}
             loading={'lazy'}
+            onPointerDown={(e) => e.preventDefault()}
             // Subtract 1rem * (number of flex items) from width calculation
             // to account for gaps between items
-            onMouseDown={(e) => e.preventDefault()}
             className="h-80 w-full flex-shrink-0 rounded object-cover md:w-[calc((100%-1rem*1)/2)] lg:w-[calc((100%-1rem*2)/3)]"
           ></img>
         ))}
@@ -137,7 +141,7 @@ const Slider = () => {
         <button
           type="button"
           onClick={() => handleSwipe(calculateScrollAfterSwipeRight)}
-          className="absolute right-0 aspect-square w-8 translate-x-1/2 cursor-pointer self-center rounded-full bg-neutral-100 text-neutral-700 transition hover:bg-indigo-500 hover:text-white"
+          className="absolute right-0 flex aspect-square w-8 translate-x-1/2 cursor-pointer items-center justify-center self-center rounded-full bg-neutral-100 text-neutral-700 transition hover:bg-indigo-500 hover:text-white"
         >
           <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
         </button>
